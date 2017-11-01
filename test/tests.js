e2d2f02d8f1059ad5c53f9890ab26588256730f9
nigelflippo <flippo.nigel@gmail.com>
2017-11-01 12:01:51 -0600

    Added browser tests

---
+++ test/tests.js
@@ -0,0 +1,18 @@
+const expect = chai.expect
+
+describe('game', function() {
+  it('should be an object', function() {
+    expect(game).to.be.an('object')
+  })
+})
+
+describe('startGame', function() {
+  it('should be a function', function() {
+    expect(restart).to.be.a('function')
+  })
+})
+describe('restart', function() {
+  it('should be a function', function() {
+    expect(restart).to.be.a('function')
+  })
+})
